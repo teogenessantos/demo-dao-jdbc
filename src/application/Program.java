7f7189216dd8a5d9c9eb4765943a00b07b738318
Teogenes <teogenes.santos@gmail.com>
2020-04-15 12:11:53 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.print(obj);
+		Seller seller = new Seller(1,"Teogenes","teogenes.santos@gmail.com",new Date(), 5000.0, obj);
+		
+		System.out.print(seller);
 	}
 
 }

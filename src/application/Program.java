7d753c12738691bcaaf13fcbc0a1d0d6a96b7692
Teogenes <teogenes.santos@gmail.com>
2020-04-15 12:49:22 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(1,"Teogenes","teogenes.santos@gmail.com",new Date(), 5000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.print(seller);
 	}

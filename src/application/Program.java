271f8e8e8b273f43d6f3b9cb2fa311ddaed4eebd
Teogenes <teogenes.santos@gmail.com>
2020-04-15 16:11:59 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,15 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(1,"Teogenes","teogenes.santos@gmail.com",new Date(), 5000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.print(seller);
 	}

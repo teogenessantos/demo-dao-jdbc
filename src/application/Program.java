df42ffa96b5a0e285352894fddc8212037b64f7f
Teogenes <teogenes.santos@gmail.com>
2020-04-16 10:11:44 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,10 +29,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n====== Test Saller Insert method ======");
+		/*System.out.println("\n====== Test Saller Insert method ======");
 		Seller newSeller = new Seller(null, "Tobias", "tobias@gmail.com", new Date(), 5520.0, new Department(2, null)); 
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = "+newSeller.getId());
+		System.out.println("Inserted! New id = "+newSeller.getId());*/
+		
+		System.out.println("\n====== Test Saller Update method ======");
+		seller = sellerDao.findById(7);
+		seller.setName("Teógenes Santos");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
